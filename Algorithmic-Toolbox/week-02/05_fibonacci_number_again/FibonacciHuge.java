11719cd62e47c28c7d2086351d47593e03237890
kremlevmax <kremlevmax.webdev@icloud.com>
2023-02-01 22:53:15 -0800

    Half way large fib numbers

--- Algorithmic-Toolbox/week-02/05_fibonacci_number_again/FibonacciHuge.java
+++ Algorithmic-Toolbox/week-02/05_fibonacci_number_again/FibonacciHuge.java
@@ -10,18 +10,47 @@ public class FibonacciHuge {
 
         for (long i = 0; i < n - 1; ++i) {
             long tmp_previous = previous;
-            previous = current%m;
-            current = (tmp_previous + current)%m;
+            previous = current;
+            current = (tmp_previous + current);
         }
 
         return current;
+    }
+
+    private static long[][] multiplyMatrix(long[][] matrix1, long[][] matrix2) {
+        long x1 = matrix1[0][0]*matrix2[0][0] + matrix1[1][0]*matrix2[0][1];
+        long y1 = matrix1[0][1]*matrix2[0][0] + matrix1[1][1]*matrix2[0][1];
+        long x2 = matrix1[0][0]*matrix2[1][0] + matrix1[1][0]*matrix2[1][1];
+        long y2 = matrix1[0][1]*matrix2[1][0] + matrix1[1][1]*matrix2[1][1];
+
+        return new long[][]{{x1, x2}, {y1, y2}};
+    }
+
+
+    private static long[][] matrixPower(long[][] matrix, long power) {
+        long[][] result = matrix;
+
+        for (long i = 0; i < power ; i++) {
+            result = multiplyMatrix(result, matrix);
+        }
+        return result;
+    }
+
+    private static long getFibonacciHuge(long number, long divisor) {
+        long[][] qMatrix = new long[][]{{0,1}, {1, 1}};
+
+        if (number < 2) {
+            return number;
+        }
+
+        return matrixPower(qMatrix, number)[0][0];
     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
-        System.out.println(getFibonacciHugeNaive(n, m));
+        System.out.println(getFibonacciHuge(n, m));
     }
 }
 
